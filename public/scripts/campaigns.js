7a531e6146ec7efddc701688f88caa7c2c9f0935
ds526-student <damionsklenarsclare@gmail.com>
2026-07-06 15:09:51 +0100

    featured update
    featured updated images
    
    object created for work experience

---
+++ public/scripts/campaigns.js
@@ -0,0 +1,50 @@
+// work experience, used to generate work experience cards
+const work_experience = [
+    {
+        title: "Software Engineer",
+        company: "Farmland Efficiency",
+        Location: "Hamilton, NZ",
+        dates: "October 2025 - March 2026",
+        type: "Full-time",
+        description: 
+            `Joined an early stage startup as a software engineer building an AI-driven blueberry harvesting system,
+            using computer vision to improve harvesting efficiency and displaying real-time data.`,
+        achievements: [
+            "Developed live data transfer pipeline from sensor data to company servers",
+            "Developed a web application to display real-time data from the harvesting system",
+            "Developed early computer vision system to detect and classify blueberries"
+        ],
+        stack: ["Python", "Java", "Node.js", "Express", "PostgreSQL", "Redis", "Docker"]
+    },
+    {
+        title: "Information Technology Intern",
+        company: "New Zealand Thoroughbred Racing",
+        Location: "Cambridge, NZ",
+        dates: "June - July 2025",
+        type: "Internship",
+        description:
+            `Embedded within the team infrastructure, troubleshooting and monitoring live systems
+            while contributing to improving internal applications and processes.`,
+        achievements: [
+            "Built and configured API monitors that improve reliability tracking and alerting",
+            "Improved upon internal web application for choosing and managing jockey silk designs",
+            "Presented project outcomes and improvements live to two departments"
+        ],
+        stack: ["Python", "Node.js", "React"]
+    },
+    {
+        title: "Long Life Assistant",
+        company: "WoolWorths New Zealand",
+        Location: "Hamilton, NZ",
+        dates: "October 2022 - October 2025",
+        type: "Part-time",
+        description:
+            `Worked across checkouts and long-life departments, assisting customers and
+            with stock operations and display management.`,
+        achievements: [
+            "Recognised for consistent friendly, and efficient customer service",
+            "Successfully rolling out new weekly promotional campaigns and displays across the store",
+        ],
+        stack: ["Customer Service", "Stock Management", "Display Management"]
+    }
+]
--- public/scripts/featured.js
+++ public/scripts/featured.js
@@ -15,7 +15,7 @@ const featured_projects = [
         repo: "https://github.com/noimadd/sheetsync"
     },
     {
-        img: "/assets/featured/placeholder.png",
+        img: "/assets/featured/portfolio.png",
         title: "Personal Portfolio",
         type: "Web Application",
         stage: "In Development",
@@ -43,7 +43,7 @@ const featured_projects = [
         repo: "https://github.com/noimadd/trialOfRealms"
     },
     {
-        img: "/assets/featured/placeholder.png",
+        img: "/assets/featured/suppsense.png",
         title: "SuppSense",
         type: "Web Application",
         stage: "Completed Prototype",
@@ -56,16 +56,16 @@ const featured_projects = [
         repo: "https://github.com/noimadd/samSuppSense"
     },
     {
-        img: "/assets/featured/placeholder.png",
-        title: "Trial of Realms 2.0",
-        type: "Game",
-        stage: "Planned",
+        img: "/assets/featured/racecar.png",
+        title: "Digital Dashboard",
+        type: "Embedded System",
+        stage: "Planned Prototype",
         version: "0.0.0",
         description:
-           `Trial of Realms 2.0 is a planned remake of the original Trial of Realms game,
-            with a focus on developing a user interface, alongside improving the combat system, skills, and items,
-            while also adding new content. Development of this project is planned to begin after the completion of SheetSync.`,
-        stack: ["Node.js", "EJS", "Express", "Socket.io", "PostgreSQL", "Redis", "Docker"],
+            `The Digital Dashboard is a planned embedded system project designed to provide users with a digital interface for their race car.
+            It will feature a variety of sensors and displays providing real-time information about the car's performance. It will be designed to be 
+            easily integrated into existing race cars, and updates can be done easily through a USB port.`,
+        stack: ["Rust", "ESP32", "CAN Bus", "Node.js", "Express", "EJS", "PostgreSQL"],
         live: '',
         repo: ''
     },
